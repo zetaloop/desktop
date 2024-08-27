e464a28ae89ed39d7e729396a874aaf5e9d183ea
zetaloop <zetaloop@outlook.com>
2024-08-27 20:34:08 +0800

    更新文件地址

--- script/dist-info.ts
+++ script/dist-info.ts
@@ -142,9 +142,11 @@ export function getUpdatesURL() {
   //const architecturePath = getDistArchitecture() === 'arm64' ? 'arm64/' : ''
   //return `https://central.github.com/api/deployments/desktop/desktop/${architecturePath}latest?version=${version}&env=${getChannel()}`
   if (process.platform === 'win32') {
-    return `http://localhost:8080/Desktop-metadata/win32-${getDistArchitecture()}-${getChannel()}-latest.txt`
+    return `http://localhost:8080/Desktop-metadata/win32-${getDistArchitecture()}-${getChannel()}`
+    // example: http://localhost:8080/Desktop-metadata/win32-x64-production/RELEASES
   } else if (process.platform === 'darwin') {
-    return `http://localhost:8080/Desktop-metadata/darwin-${getDistArchitecture()}-${getChannel()}-latest.json`
+    return `http://localhost:8080/Desktop-metadata/darwin-${getDistArchitecture()}-${getChannel()}/releases.json`
+    // example: http://localhost:8080/Desktop-metadata/darwin-arm64-production/releases.json
   } else {
     throw new Error(`No updates available for platform: ${process.platform}`)
   }
