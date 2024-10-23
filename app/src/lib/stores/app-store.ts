b8eb0c776acc0ece9f4f044805ad7b272f4ba008
zetaloop <zetaloop@outlook.com>
2024-10-24 02:33:48 +0800

    优化 "HEAD 指针分离" 的表述

--- app/src/lib/stores/app-store.ts
+++ app/src/lib/stores/app-store.ts
@@ -4505,7 +4505,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
     }
 
     if (tip.kind === TipState.Detached) {
-      throw new Error('当前仓库处于游离 HEAD 指针状态。')
+      throw new Error('当前仓库 HEAD 指针分离。')
     }
 
     if (tip.kind === TipState.Valid) {
@@ -4751,7 +4751,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
       }
 
       if (tip.kind === TipState.Detached) {
-        throw new Error('当前仓库处于游离 HEAD 指针状态。')
+        throw new Error('当前仓库 HEAD 指针分离。')
       }
 
       if (tip.kind === TipState.Valid) {
