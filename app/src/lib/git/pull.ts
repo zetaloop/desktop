0c626174c43864ce974d2db7862f2bf27949d591
Markus Olsson <j.markus.olsson@gmail.com>
2024-11-11 13:43:24 +0100

    Get rid of redundant GitError throw

--- app/src/lib/git/pull.ts
+++ app/src/lib/git/pull.ts
@@ -1,13 +1,7 @@
-import {
-  git,
-  GitError,
-  gitRebaseArguments,
-  IGitStringExecutionOptions,
-} from './core'
+import { git, gitRebaseArguments, IGitStringExecutionOptions } from './core'
 import { Repository } from '../../models/repository'
 import { IPullProgress } from '../../models/progress'
 import { PullProgressParser, executionOptionsWithProgress } from '../progress'
-import { AuthenticationErrors } from './authentication'
 import { enableRecurseSubmodulesFlag } from '../feature-flag'
 import { IRemote } from '../../models/remote'
 import { envForRemoteOperation } from './environment'
@@ -48,7 +42,6 @@ export async function pull(
 ): Promise<void> {
   let opts: IGitStringExecutionOptions = {
     env: await envForRemoteOperation(remote.url),
-    expectedErrors: AuthenticationErrors,
   }
 
   if (progressCallback) {
@@ -89,11 +82,7 @@ export async function pull(
   }
 
   const args = await getPullArgs(repository, remote.name, progressCallback)
-  const result = await git(args, repository.path, 'pull', opts)
-
-  if (result.gitErrorDescription) {
-    throw new GitError(result, args)
-  }
+  await git(args, repository.path, 'pull', opts)
 }
 
 /**
