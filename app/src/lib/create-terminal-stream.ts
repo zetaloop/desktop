8b0463f3bc680413247e9f5172eec86fd92806f7
Markus Olsson <j.markus.olsson@gmail.com>
2024-11-13 16:13:06 +0100

    Document it

--- app/src/lib/create-terminal-stream.ts
+++ app/src/lib/create-terminal-stream.ts
@@ -1,5 +1,25 @@
 import { Transform } from 'node:stream'
 
+/**
+ * Creates a transform stream which removes redundant Git progress output by
+ * handling carriage returns the same way a terminal would, i.e by
+ * moving the cursor and (potentially) overwriting text.
+ *
+ * Git (and many other CLI tools) use this trick to present the
+ * user with nice looking progress. When writing something like...
+ *
+ * 'Downloading: 1%  \r'
+ * 'Downloading: 2%  \r'
+ *
+ * ...to the terminal the user is gonna perceive it as if the 1 just
+ * magically changes to a two.
+ *
+ * The carriage return character for all of you kids out there
+ * that haven't yet played with a manual typewriter refers to the
+ * "carriage" which held the character arms, see
+ *
+ *  https://en.wikipedia.org/wiki/Carriage_return#Typewriters
+ */
 export const createTerminalStream = () => {
   // The virtual line buffer, think of this as one long line (1 KiB) in a
   // terminal where `l` is the farthest we've written in that line and `p` is
