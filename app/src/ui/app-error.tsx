9c949f04744c3f1ffa4b63071af6f01d99959346
Markus Olsson <j.markus.olsson@gmail.com>
2024-11-14 16:35:13 +0100

    Move parsing of file names into presentation layer

--- app/src/ui/app-error.tsx
+++ app/src/ui/app-error.tsx
@@ -7,7 +7,7 @@ import {
   DefaultDialogFooter,
 } from './dialog'
 import { dialogTransitionTimeout } from './app'
-import { GitError, isAuthFailureError } from '../lib/git/core'
+import { coerceToString, GitError, isAuthFailureError } from '../lib/git/core'
 import { Popup, PopupType } from '../models/popup'
 import { OkCancelButtonGroup } from './dialog/ok-cancel-button-group'
 import { ErrorWithMetadata } from '../lib/error-with-metadata'
@@ -15,6 +15,9 @@ import { RetryActionType, RetryAction } from '../models/retry-actions'
 import { Ref } from './lib/ref'
 import memoizeOne from 'memoize-one'
 import { parseCarriageReturn } from '../lib/parse-carriage-return'
+import { GitError as DugiteError } from 'dugite'
+import { LinkButton } from './lib/link-button'
+import { getFileFromExceedsError } from '../lib/helpers/regex'
 
 interface IAppErrorProps {
   /** The error to be displayed  */
@@ -98,12 +101,47 @@ export class AppError extends React.Component<IAppErrorProps, IAppErrorState> {
       return <p className="monospace">{formattedMessage}</p>
     }
 
+    if (
+      isGitError(e) &&
+      e.result.gitError === DugiteError.PushWithFileSizeExceedingLimit
+    ) {
+      const files = getFileFromExceedsError(coerceToString(e.result.stderr))
+      return (
+        <>
+          <p>{error.message}</p>
+          {files.length > 0 && (
+            <>
+              <p>Files that exceed the limit</p>
+              <ul>
+                {files.map(file => (
+                  <li key={file}>{file}</li>
+                ))}
+              </ul>
+            </>
+          )}
+          <p>
+            See{' '}
+            <LinkButton uri="https://gh.io/lfs">https://gh.io/lfs</LinkButton>{' '}
+            for more information on managing large files on GitHub
+          </p>
+        </>
+      )
+    }
+
     return <p>{e.message}</p>
   }
 
   private getTitle(error: Error) {
-    if (isCloneError(error)) {
-      return 'Clone failed'
+    switch (getDugiteError(error)) {
+      case DugiteError.PushWithFileSizeExceedingLimit:
+        return 'File size limit exceeded'
+    }
+
+    switch (getRetryActionType(error)) {
+      case RetryActionType.Clone:
+        return 'Clone failed'
+      case RetryActionType.Push:
+        return 'Failed to push'
     }
 
     return 'Error'
@@ -268,4 +306,17 @@ function isCloneError(error: Error) {
   }
   const { retryAction } = error.metadata
   return retryAction !== undefined && retryAction.type === RetryActionType.Clone
+}
+
+function getRetryActionType(error: Error) {
+  if (!isErrorWithMetaData(error)) {
+    return undefined
+  }
+
+  return error.metadata.retryAction?.type
+}
+
+function getDugiteError(error: Error) {
+  const e = getUnderlyingError(error)
+  return isGitError(e) ? e.result.gitError : undefined
 }
