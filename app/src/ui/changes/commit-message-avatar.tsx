f8875b66051bf5f20a98262f0e32012eebfcd451
Markus Olsson <j.markus.olsson@gmail.com>
2025-02-05 16:34:39 +0100

    Hide email quick select if no email are available

--- app/src/ui/changes/commit-message-avatar.tsx
+++ app/src/ui/changes/commit-message-avatar.tsx
@@ -279,24 +279,29 @@ export class CommitMessageAvatar extends React.Component<
       </>
     )
 
+    const hasEmails = this.props.accountEmails.length > 0
+
     const sharedFooter = (
       <>
-        <Row>
-          <Select
-            label="Your Account Emails"
-            value={this.state.accountEmail}
-            onChange={this.onSelectedGitHubEmailChange}
-          >
-            {this.props.accountEmails.map(n => (
-              <option key={n} value={n}>
-                {n}
-              </option>
-            ))}
-          </Select>
-        </Row>
+        {hasEmails && (
+          <Row>
+            <Select
+              label="Your Account Emails"
+              value={this.state.accountEmail}
+              onChange={this.onSelectedGitHubEmailChange}
+            >
+              {this.props.accountEmails.map(n => (
+                <option key={n} value={n}>
+                  {n}
+                </option>
+              ))}
+            </Select>
+          </Row>
+        )}
         <Row>
           <div className="secondary-text">
-            You can also choose an email local to this repository from the{' '}
+            You can{hasEmails ? ' also' : ''} choose an email local to this
+            repository from the{' '}
             <LinkButton onClick={this.onRepositorySettingsClick}>
               repository settings
             </LinkButton>
@@ -307,9 +312,11 @@ export class CommitMessageAvatar extends React.Component<
           <Button onClick={this.onIgnoreClick} type="button">
             Ignore
           </Button>
-          <Button onClick={this.onUpdateEmailClick} type="submit">
-            {updateEmailTitle}
-          </Button>
+          {hasEmails && (
+            <Button onClick={this.onUpdateEmailClick} type="submit">
+              {updateEmailTitle}
+            </Button>
+          )}
         </Row>
       </>
     )
