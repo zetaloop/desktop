95dae35c58ace50eb9f683d28449d36841d5c60e
zetaloop <zetaloop@outlook.com>
2025-03-10 15:04:57 +0800

    新的允许登录多个企业版账号

--- app/src/ui/preferences/accounts.tsx
+++ app/src/ui/preferences/accounts.tsx
@@ -68,7 +68,7 @@ export class Accounts extends React.Component<IAccountsProps, {}> {
           this.renderSignIn(SignInType.Enterprise)
         ) : (
           <Button onClick={this.props.onEnterpriseSignIn}>
-            Add GitHub Enteprise account
+            添加 GitHub 企业版账号
           </Button>
         )}
       </>
