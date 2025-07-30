13c233363901f86400135399099e1f4538d062dc
zetaloop <zetaloop@outlook.com>
2025-07-31 01:00:32 +0800

    fix: 强制推送警告文案优化

--- app/src/ui/rebase/confirm-force-push.tsx
+++ app/src/ui/rebase/confirm-force-push.tsx
@@ -45,7 +45,7 @@ export class ConfirmForcePush extends React.Component<
         <DialogContent>
           <p>
             强制推送将会重写 <Ref>{this.props.upstreamBranch}</Ref>{' '}
-            的历史记录。正在使用该分支的人们必须得重置他们的本地分支才能匹配远程历史记录了，这可能会造成一些麻烦。
+            的历史记录。其他人需要重置本地分支才能匹配这个分支的远程历史记录，这可能会造成一些麻烦。
           </p>
           <div>
             <Checkbox
