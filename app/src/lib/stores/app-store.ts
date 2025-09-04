01413496bd7966317914c2e4b7511593bd975182
zetaloop <zetaloop@outlook.com>
2025-09-05 00:25:21 +0800

    feat: 最近五条改成最近十条

--- app/src/lib/stores/app-store.ts
+++ app/src/lib/stores/app-store.ts
@@ -5545,7 +5545,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
           const recentCommits: ReadonlyArray<Commit> = await getCommits(
             repository,
             undefined,
-            5
+            10
           )
 
           if (recentCommits.length > 0) {
