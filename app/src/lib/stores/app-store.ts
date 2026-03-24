58beafcdd57b43aad751e8e940e9f7efaff8b10f
zetaloop <zetaloop@outlook.com>
2026-03-25 02:47:23 +0800

    接入 Difftastic 差异引擎
    根据 difft 返回的 language 和 status 决定是否采用结果，Text 及 created/deleted 回退到 git diff。

--- app/src/lib/stores/app-store.ts
+++ app/src/lib/stores/app-store.ts
@@ -1891,13 +1891,16 @@ export class AppStore extends TypedBaseStore<IAppState> {
             repository,
             file,
             this.orderShasByHistory(repository, shas),
-            this.hideWhitespaceInHistoryDiff
+            this.hideWhitespaceInHistoryDiff,
+            false,
+            this.enableDifftastic && this.isDifftOnPath
           )
         : await getCommitDiff(
             repository,
             file,
             shas[0],
-            this.hideWhitespaceInHistoryDiff
+            this.hideWhitespaceInHistoryDiff,
+            this.enableDifftastic && this.isDifftOnPath
           )
 
     const stateAfterLoad = this.repositoryStateCache.get(repository)
@@ -3131,7 +3134,8 @@ export class AppStore extends TypedBaseStore<IAppState> {
     const diff = await getWorkingDirectoryDiff(
       repository,
       selectedFileBeforeLoad,
-      this.hideWhitespaceInChangesDiff
+      this.hideWhitespaceInChangesDiff,
+      this.enableDifftastic && this.isDifftOnPath
     )
 
     const stateAfterLoad = this.repositoryStateCache.get(repository)
@@ -3340,7 +3344,13 @@ export class AppStore extends TypedBaseStore<IAppState> {
       return
     }
 
-    const diff = await getCommitDiff(repository, file, file.commitish)
+    const diff = await getCommitDiff(
+      repository,
+      file,
+      file.commitish,
+      false,
+      this.enableDifftastic && this.isDifftOnPath
+    )
 
     const stateAfterLoad = this.repositoryStateCache.get(repository)
     const changesStateAfterLoad = stateAfterLoad.changesState
@@ -8496,7 +8506,8 @@ export class AppStore extends TypedBaseStore<IAppState> {
             baseBranch.name,
             currentBranch.name,
             this.hideWhitespaceInPullRequestDiff,
-            commitSHAs[0]
+            commitSHAs[0],
+            this.enableDifftastic && this.isDifftOnPath
           )
         )) ?? null
 
